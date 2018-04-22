27aab3a332405eb5a84e0078452770aea6ed8b0a
James Geater <jgeater@outlook.com>
2018-04-22 14:41:05 -0400

    initial commit

--- RbtCmdr/MainWindow.xaml.cs
+++ RbtCmdr/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace RbtCmdr
 {
@@ -19,9 +22,67 @@ namespace RbtCmdr
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool LockWorkStation();
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        [DllImport("user32")]
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(LockWS.IsChecked==true)
+            {
+                LockWorkStation();
+            }
+
+            if (sleep.IsChecked ==true)
+            {
+                System.Windows.Forms.Application.SetSuspendState(PowerState.Suspend, true, true);
+            }
+
+            if (Hib1.IsChecked==true)
+            {
+                System.Windows.Forms.Application.SetSuspendState(PowerState.Hibernate, true, true);
+            }
+
+            if(sdown.IsChecked ==true)
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+            if (Rbt.IsChecked ==true)
+            {
+                Process.Start("shutdown", "/r /t 0");
+            }
+            if(Log_off.IsChecked ==true)
+            {
+                ExitWindowsEx(0, 0);
+            }
+            if (RBT_FW.IsChecked ==true)
+            {
+                Process.Start("shutdown", "/r /fw /t 0");
+            }
+            if(Rbt_Adv.IsChecked==true)
+            {
+                Process.Start("shutdown", "/r /o /t 0");
+            }
         }
     }
+
+
 }
